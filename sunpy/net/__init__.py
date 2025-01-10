10cfd886361bc1e18db2d91a308fa9849ce3c498
tanishy7777 <23110328@iitgn.ac.in>
2025-01-11 03:58:51 +0530

    Adds FakeClient to reproduce work and error property in UnifiedResponse

--- sunpy/net/__init__.py
+++ sunpy/net/__init__.py
@@ -13,5 +13,6 @@ from sunpy.net import jsoc as _
 from sunpy.net import vso as _
 from sunpy.net.fido_factory import Fido
 from sunpy.net.scraper import Scraper
+from fake_client import ExampleClient as _    
 
 __all__ = ["Fido", "Scraper"]
